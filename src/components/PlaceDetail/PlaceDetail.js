6ac79d368abaec15784d37e409edaeff63e5ab65
Eduardo Reche <edu.reche@gmail.com>
2019-01-23 21:48:35 -0200

    Added images and modal

---
+++ src/components/PlaceDetail/PlaceDetail.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Modal, View, Image, Text, Button, StyleSheet } from 'react-native';
+
+const placeDetail = ({ selectedPlace, onItemDeleted, onModalClosed }) => {
+  let modalContent = null;
+
+  if (selectedPlace) {
+    const { value, image } = selectedPlace;
+    modalContent = (
+      <View>
+        <Image source={image} style={styles.placeImage} />
+        <Text style={styles.placeName}>{value}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <Modal
+      onRequestClose={onModalClosed}
+      visible={selectedPlace !== null}
+      animationType="slide"
+    >
+      <View style={styles.modalContainer}>
+        {modalContent}
+        <View>
+          <Button title="Delete" color="red" onPress={onItemDeleted} />
+          <Button title="Close" onPress={onModalClosed} />
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    margin: 22
+  },
+  placeImage: {
+    width: '100%',
+    height: 200
+  },
+  placeName: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 28
+  }
+});
+
+export default placeDetail;
