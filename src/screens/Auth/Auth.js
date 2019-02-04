d743a9de34cc15448cb9a02b0fa63d40a476f16d
Eduardo Reche <edu.reche@gmail.com>
2019-02-03 22:14:48 -0200

    Change view when rotate

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, ImageBackground } from 'react-native';
+import { View, StyleSheet, ImageBackground, Dimensions } from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
@@ -9,17 +9,51 @@ import ButtonWithBackground from '../../components/UI/ButtonWithBackground/Butto
 import backgroundImage from '../../assets/background.jpg';
 
 class AuthScreen extends Component {
+  state = {
+    respStyles: {
+      pwContainerDirection: 'column',
+      pwContainerJustifyContent: 'flex-start',
+      pwWrapperWidth: '100%'
+    }
+  };
+
+  constructor(props) {
+    super(props);
+
+    Dimensions.addEventListener('change', dims => {
+      this.setState({
+        respStyles: {
+          pwContainerDirection:
+            Dimensions.get('window').height > 500 ? 'column' : 'row',
+          pwContainerJustifyContent:
+            Dimensions.get('window').height > 500
+              ? 'flex-start'
+              : 'space-between',
+          pwWrapperWidth: Dimensions.get('window').height > 500 ? '100%' : '45%'
+        }
+      });
+    });
+  }
+
   loginHandler = () => {
     startMainTabs();
   };
 
   render() {
+    let headingText = null;
+
+    if (Dimensions.get('window').height > 600) {
+      headingText = (
+        <MainText>
+          <HeadingText>Please Log In</HeadingText>
+        </MainText>
+      );
+    }
+
     return (
       <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
         <View style={styles.container}>
-          <MainText>
-            <HeadingText>Please Log In</HeadingText>
-          </MainText>
+          {headingText}
           <ButtonWithBackground color="#29aaf4" onPress={() => alert('hello')}>
             Switch to Login
           </ButtonWithBackground>
@@ -29,8 +63,30 @@ class AuthScreen extends Component {
               placeholder="Your email address"
               style={styles.input}
             />
-            <DefaultInput placeholder="Password" style={styles.input} />
-            <DefaultInput placeholder="Confirm Password" style={styles.input} />
+            <View
+              style={{
+                flexDirection: this.state.respStyles.pwContainerDirection,
+                justifyContent: this.state.respStyles.pwContainerJustifyContent
+              }}
+            >
+              <View
+                style={{
+                  width: this.state.respStyles.pwWrapperWidth
+                }}
+              >
+                <DefaultInput placeholder="Password" style={styles.input} />
+              </View>
+              <View
+                style={{
+                  width: this.state.respStyles.pwWrapperWidth
+                }}
+              >
+                <DefaultInput
+                  placeholder="Confirm Password"
+                  style={styles.input}
+                />
+              </View>
+            </View>
           </View>
 
           <ButtonWithBackground onPress={this.loginHandler} color="#29aaf4">
@@ -58,6 +114,13 @@ const styles = StyleSheet.create({
   input: {
     backgroundColor: '#eee',
     borderColor: '#bbb'
+  },
+  passwordContainer: {
+    flexDirection: Dimensions.get('window').height > 500 ? 'column' : 'row',
+    justifyContent: 'space-between'
+  },
+  passwordWrapper: {
+    width: Dimensions.get('window').height > 500 ? '100%' : '45%'
   }
 });
 
