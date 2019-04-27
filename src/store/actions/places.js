5240765e65bdd055102ce0dd7989ffb7120f69ad
Eduardo Reche <edu.reche@gmail.com>
2019-04-27 12:31:47 -0300

    Add data to firebase

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,12 +1,20 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
-export const addPlace = (name, location, image) => {
-  return {
-    type: ADD_PLACE,
+export const addPlace = (name, location, image) => dispatch => {
+  const placeData = {
     name,
-    location,
-    image
+    location
   };
+
+  fetch('https://awesome-places-b54de.firebaseio.com/places.json', {
+    method: 'POST',
+    body: JSON.stringify(placeData)
+  })
+    .catch(err => console.log(err))
+    .then(res => res.json())
+    .then(parsedRes => {
+      console.log(parsedRes);
+    });
 };
 
 export const deletePlace = key => {
