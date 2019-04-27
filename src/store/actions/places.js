8615cbec9d616ee57af437d6ea7cf752d5cded03
Eduardo Reche <edu.reche@gmail.com>
2019-04-27 13:46:18 -0300

    Added function to store image

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,19 +1,28 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (name, location, image) => dispatch => {
-  const placeData = {
-    name,
-    location
-  };
-
-  fetch('https://awesome-places-b54de.firebaseio.com/places.json', {
+  fetch('https://us-central1-awesome-places-b54de.cloudfunctions.net/storeImage', {
     method: 'POST',
-    body: JSON.stringify(placeData)
+    body: JSON.stringify({ image: image.base64 })
   })
     .catch(err => console.log(err))
     .then(res => res.json())
     .then(parsedRes => {
-      console.log(parsedRes);
+      const placeData = {
+        name,
+        location,
+        image: parsedRes.imageUrl
+      };
+
+      return fetch('https://awesome-places-b54de.firebaseio.com/places.json', {
+        method: 'POST',
+        body: JSON.stringify(placeData)
+      })
+        .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(parsedRes => {
+          console.log(parsedRes);
+        });
     });
 };
 
